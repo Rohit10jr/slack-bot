6563af13a3c1fb4a3217bfe30f326b4fbf7dff9e
Rohit10jr <rohitjworkspace@gmail.com>
2024-02-27 22:21:31 +0530

    complete

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
+import string
+from datetime import datetime, timedelta
+import time
 
 env_path = Path(".") / (".env")
 load_dotenv(dotenv_path=env_path)
@@ -20,6 +23,15 @@ BOT_ID = client.api_call("auth.test")['user_id']
 
 message_counts = {}
 welcome_messages = {}
+
+BAD_WORDS = ['hmm', 'no', 'tim']
+
+SCHEDULED_MESSAGES = [
+    {'text': 'First message', 'post_at': (
+        datetime.now() + timedelta(seconds=20)).timestamp(), 'channel': 'C01BXQNT598'},
+    {'text': 'Second Message!', 'post_at': (
+        datetime.now() + timedelta(seconds=30)).timestamp(), 'channel': 'C01BXQNT598'}
+]
 
 class WelcomeMessage:
     START_TEXT = {
@@ -65,18 +77,50 @@ class WelcomeMessage:
 
         return {'type':'section', 'text':{'type':'mrkdwn', 'text':text}}
     
-def send_welcome_message(channel, user):
+def send_welcome_message(channel, user):    
+    if channel not in welcome_messages:
+        welcome_messages[channel] = {}
+    if user in welcome_messages[channel]:
+        return
     welcome = WelcomeMessage(channel, user)
     message = welcome.get_message()
     response = client.chat_postMessage(**message)
     welcome.timestamp = response['ts']
 
-    if channel not in welcome_messages:
-        welcome_messages[channel] = {}
     welcome_messages[channel][user] = welcome
 
+def list_scheduled_messages(channel):
+    response = client.chat_scheduledMessages_list(channel=channel)
+    messages = response.data.get('scheduled_messages')
+    ids = []
+    for msg in messages:
+        ids.append(msg.get('id'))
 
+    return ids
 
+def schedule_messages(messages):
+    ids = []
+    for msg in messages:
+        response = client.chat_scheduleMessage(
+            channel=msg['channel'], text=msg['text'], post_at=msg['post_at']).data
+        id_ = response.get('scheduled_message_id')
+        ids.append(id_)
+
+    return ids
+
+def delete_scheduled_messages(ids, channel):
+    for _id in ids:
+        try:
+            client.chat_deleteScheduledMessage(
+                channel=channel, scheduled_message_id=_id)
+        except Exception as e:
+            print(e)
+
+def check_if_bad_words(message):
+    msg = message.lower()
+    msg = msg.translate(str.maketrans('', '', string.punctuation))
+
+    return any(word in msg for word in BAD_WORDS)
 
 @slack_event_adapter.on('message')
 def message(payload):
@@ -86,7 +130,6 @@ def message(payload):
     user_id = event.get('user')
     text = event.get('text')
 
-    # if user_id != None and BOT_ID != user_id:
     if user_id != None and BOT_ID != user_id:
         if user_id in message_counts:
             message_counts[user_id] += 1
@@ -95,7 +138,28 @@ def message(payload):
 
         if text.lower() == 'start':
             send_welcome_message(f'@{user_id}', user_id)
-        # client.chat_postMessage(channel=channel_id, text=text)
+        elif check_if_bad_words(text):
+            ts = event.get('ts')
+            client.chat_postMessage(
+                channel=channel_id, thread_ts=ts, text="THAT IS A BAD WORD!")
+    # send_welcome_message(f'@{user_id}', user_id)
+    # client.chat_postMessage(channel=channel_id, text=text)
+
+@ slack_event_adapter.on('reaction_added')
+def reaction(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('item', {}).get('channel')
+    user_id = event.get('user')
+
+    if f'@{user_id}' not in welcome_messages:
+        return
+
+    welcome = welcome_messages[f'@{user_id}'][user_id]
+    welcome.completed = True
+    welcome.channel = channel_id
+    message = welcome.get_message()
+    updated_message = client.chat_update(**message)
+    welcome.timestamp = updated_message['ts']
 
 # @app.route('/message-count/', methods=['GET','POST'])
 @app.route('/message-count/', methods=['POST'])
